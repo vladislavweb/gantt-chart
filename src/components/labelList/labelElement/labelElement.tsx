ab3042692aa4383e2b16ca78747fd7d0baf41a81
Vlad Ivanov <himametsu@gmail.com>
2022-11-23 14:23:29 +0800

    update tree view

--- src/components/labelList/labelElement/labelElement.tsx
+++ src/components/labelList/labelElement/labelElement.tsx
@@ -1,23 +1,55 @@
-import { FC, useCallback, useEffect, useState } from "react";
-import { Chart } from "../../../providers";
+import { FC, useCallback, useState } from "react";
+import { EnrichedChart } from "../../../utils";
 import "./labelElement.scss";
 
 import CollapseArrow from "../../../assets/images/svg/collapse_arrow.svg";
+import FirstLevelIcon from "../../../assets/images/svg/level_1_icon.svg";
+import SecondLevelIcon from "../../../assets/images/svg/level_2_icon.svg";
+import ThirdLevelIcon from "../../../assets/images/svg/level_3_icon.svg";
+import FourthLevelIcon from "../../../assets/images/svg/level_4_icon.svg";
+import FifthLevelIcon from "../../../assets/images/svg/level_5_icon.svg";
 
 interface Props {
-  data: Chart;
+  data: EnrichedChart;
 }
 
 const LabelElement: FC<Props> = ({ data }) => {
+  const { level } = data;
+
   const [isOpen, setIsOpen] = useState(true);
 
   const toggle = useCallback(() => {
     setIsOpen((value) => !value);
   }, [isOpen]);
 
+  const getLevelIcon = () => {
+    switch (level) {
+      case 1: {
+        return FirstLevelIcon;
+      }
+      case 2: {
+        return SecondLevelIcon;
+      }
+      case 3: {
+        return ThirdLevelIcon;
+      }
+      case 4: {
+        return FourthLevelIcon;
+      }
+      default: {
+        return FifthLevelIcon;
+      }
+    }
+  };
+
   return (
     <>
-      <li className="label-element">
+      <li
+        className="label-element"
+        style={{
+          paddingLeft: level ? 20 * level : 20,
+        }}
+      >
         {data.sub && (
           <button className="label-element__collapse-button" onClick={toggle}>
             <img src={CollapseArrow} alt="collapse button" />
@@ -26,7 +58,12 @@ const LabelElement: FC<Props> = ({ data }) => {
 
         {data.title && (
           <>
-            {/* <img className="label-element__icon" src="" alt="label icon" /> */}
+            <img
+              className="label-element__icon"
+              src={getLevelIcon()}
+              alt="label icon"
+              style={{ marginLeft: data.sub ? 0 : 16 }}
+            />
 
             <span className="label-element__counter">{data.sub?.length || 0}</span>
 
