b4b824c5182d19ae8b0f536750f40058495b46d2
Vlad Ivanov <himametsu@gmail.com>
2022-11-23 17:05:55 +0800

    update enrich method

--- src/utils/enrichData.ts
+++ src/utils/enrichData.ts
@@ -3,6 +3,8 @@ import { Chart, Data } from "../providers/data";
 export interface EnrichedChart extends Chart {
   sub?: EnrichedChart[];
   level?: number;
+  index?: number;
+  priority?: number;
 }
 
 export interface EnrichedData extends Data {
@@ -13,14 +15,21 @@ export const enrichData = (data: Data): EnrichedData => {
   const enrichedData: EnrichedData = data;
 
   let counter = 1;
+  let index = 0;
 
   enrichedData.chart.level = counter;
+  enrichedData.chart.index = index;
+  enrichedData.chart.priority = Math.floor(Math.random() * 3);
 
   (function flat(sub) {
     if (Array.isArray(sub)) {
       counter++;
+
       sub.forEach((el) => {
+        index++;
         el.level = counter;
+        el.index = index;
+        el.priority = Math.floor(Math.random() * 3);
 
         flat(el.sub);
       });
