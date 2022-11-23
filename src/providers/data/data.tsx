47f11795bf88c831f00f346a0210a5f2381134d0
Vlad Ivanov <himametsu@gmail.com>
2022-11-23 14:21:27 +0800

    update data service

--- src/providers/data/data.tsx
+++ src/providers/data/data.tsx
@@ -1,6 +1,13 @@
 import { createContext, FC, ReactNode, useState } from "react";
+import {
+  DisplayTaskElements,
   enrichData,
   EnrichedData,
+  flatTasks,
+  sortTasksByDate,
+  taskListMapper,
+  Tasks,
+} from "../../utils";
 
 export interface Chart {
   id: number;
@@ -18,6 +25,9 @@ export interface Data {
 
 interface DataContext {
   data?: EnrichedData;
+  displayTaskElements?: DisplayTaskElements;
+  tasks?: Tasks;
+  sortedTasksByDate?: Tasks;
   loadData: () => Promise<void>;
 }
 
@@ -31,16 +41,29 @@ export const Context = createContext<DataContext>({
 
 const DataProvider: FC<Props> = ({ children }) => {
   const [data, setData] = useState<EnrichedData>();
+  const [displayTaskElements, setDisplayTaskElements] = useState<DisplayTaskElements>();
+  const [tasks, setTasks] = useState<Tasks>();
+  const [sortedTasksByDate, setSortedTasksByDate] = useState<Tasks>();
 
   const loadData = async () => {
     fetch("http://82.202.204.94/tmp/test.php")
       .then((data) => data.json())
       .then((data: Data) => {
         setData(enrichData(data));
+        const flatedTasks = flatTasks(data.chart);
+        const sortedTasks = sortTasksByDate(flatedTasks);
+        setData(data);
+        setTasks(flatedTasks);
+        setSortedTasksByDate(sortedTasks);
+        setDisplayTaskElements(taskListMapper(sortedTasks));
       });
   };
 
-  return <Context.Provider value={{ data, loadData }}>{children}</Context.Provider>;
+  return (
+    <Context.Provider value={{ data, displayTaskElements, tasks, sortedTasksByDate, loadData }}>
+      {children}
+    </Context.Provider>
+  );
 };
 
 export default DataProvider;
