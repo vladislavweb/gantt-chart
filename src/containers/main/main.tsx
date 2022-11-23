44615528c7f5bba04a96d8c7d426b2f0b75e4be4
Vlad Ivanov <himametsu@gmail.com>
2022-11-23 14:22:35 +0800

    show label list & task list

--- src/containers/main/main.tsx
+++ src/containers/main/main.tsx
@@ -1,6 +1,12 @@
 import { FC } from "react";
+import { LabelList, TaskList } from "../../components";
 import "./main.scss";
 
-const Main: FC = () => <main className="main"></main>;
+const Main: FC = () => (
+  <main className="main">
+    <LabelList />
+    <TaskList />
+  </main>
+);
 
 export default Main;
